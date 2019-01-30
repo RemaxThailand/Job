296a0a7b35e8a4641e24737027f48ede088fc3d4
ThaiSmartDev <mxfrank0003@zonemail.clpccd.edu>
2019-01-30 14:07:49 +0700

    Update Shop Serial Sync

--- objects/shopSerial.js
+++ objects/shopSerial.js
@@ -1,8 +1,10 @@
 exports.sync = function (req, res) {
     let tp = require('tedious-promises');
     tp.setConnectionConfig(require(`${__dirname}/../../.config/www/mssql.json`));
-    tp.sql(`EXEC firebase..sp_ShopSerialSync`).execute()
+    tp.sql(`EXEC Firebase..sp_ShopSerialSync`).execute()
     .then(function (results) {
-        res.json(results);
+        res.send(results);
+    }).fail(function(err) {
+        res.send(err);
     });
 }
