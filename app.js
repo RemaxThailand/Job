b57c485a1e63b52fe55c31e49dc6c3315a5e1faa
ThaiSmartDev <mxfrank0003@zonemail.clpccd.edu>
2019-01-30 12:09:38 +0700

    Test Firebase

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require('express'),
+    admin = require('firebase-admin')
+
 const app = express();
 const port = process.env.PORT | 3000;
 
-app.get('*', (req, res) => res.send(__dirname));
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(require(`${__dirname}/../.config/www/credential-power-system-api.json`)),
+        databaseURL: 'https://power-system-api.firebaseio.com'
+    });
+} catch (err) {}
+
+global.db = admin.database();
+
+app.get('*', function(req, res) {
+    db.ref(`serial/TH/0001/stock/190100000112615`).once('value', function (snapshot) {
+        res.json(snapshot.val());
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
