4c98cc4331b276fbc209e1aff7fccc4d0d222141
ThaiSmartDev <mxfrank0003@zonemail.clpccd.edu>
2019-01-31 10:37:46 +0700

    Add Shop Stock

---
+++ objects/shopStock.js
@@ -0,0 +1,89 @@
+exports.firebaseUpdate = function (req, res) {
+    deleteData(req, res);
+};
+
+function deleteData(req, res) {
+    let msg = {
+        delete: {},
+        update: {}
+    }
+    let tp = require('tedious-promises');
+    tp.setConnectionConfig(require(`${__dirname}/../../.config/www/mssql.json`));
+    tp.sql(`EXEC Firebase..sp_ShopStockSyncData 'delete'`).execute().then(function (results) {
+        let all = results.length;
+        if (all > 0) {
+            let success = 0;
+            let arr = [];
+            results.forEach(result => {
+                db.ref(`product/TH/${result.shop}/stock/${result.product}`).remove(function (error) {
+                    success++;
+                    if (error) {
+                        console.log(`Delete Product ${result.product} Error (${error})`);
+                    } else {
+                        arr.push(`'${result.shop}-${result.product}'`);
+                        //console.log(`Delete Serial ${result.id} Success (${success}/${all})`);
+                    }
+                    if (success >= all) {
+                        msg.delete = { success: true, result: arr.toString()};
+                        tp.sql(`DELETE FROM Firebase..ShopStock WHERE CONCAT(shop,'-', product) IN (${arr.toString()})`).execute();
+                        console.log(msg.delete);
+                        setTimeout(function () {
+                            updateData(req, res, msg);
+                        }, 1000);
+                    }
+                })
+            });
+        } else {
+            msg.delete = 'No Delete Data';
+            console.log(msg.delete);
+            updateData(req, res, msg);
+        }
+    }).fail(function (err) {
+        msg.delete = err;
+        console.log(msg.delete);
+        updateData(req, res, msg);
+    });
+}
+
+
+function updateData(req, res, msg) {
+    let tp = require('tedious-promises');
+    tp.setConnectionConfig(require(`${__dirname}/../../.config/www/mssql.json`));
+    tp.sql(`EXEC Firebase..sp_ShopStockSyncData 'update'`).execute().then(function (results) {
+        let all = results.length;
+        if (all > 0) {
+            let success = 0;
+            let arr = [];
+            results.forEach(result => {
+                //db.ref(`po/TH/0001/info/${result.id}/id`).set(result.id);
+                db.ref(`product/TH/${result.shop}/stock/${result.product}`).update({
+                    product: result.product
+                }, function (error) {
+                    success++;
+                    if (error) {
+                        console.log(`Update Product ${result.product} Error (${error})`);
+                    } 
+                    else {
+                        arr.push(`'${result.shop}-${result.product}'`);
+                        //console.log(`Update Serial ${result.id} Success (${success}/${all})`);
+                    }
+                    if(success >= all) {
+                        msg.update = { success: true, result: arr.toString()};
+                        res.send(msg);
+                        tp.sql(`UPDATE Firebase..ShopStock SET isSync = 1, syncDate = GETDATE() WHERE CONCAT(shop,'-', product) IN (${arr.toString()})`).execute();
+                        console.log(msg.update);
+                        return true;
+                    }
+                })
+            });
+        } else {
+            msg.update = 'No Update Data';
+            res.send(msg);
+            return true;
+        }
+    }).fail(function (err) {
+        msg.update = err;
+        res.send(msg);
+        return true;
+    });
+}
