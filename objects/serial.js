e4155ff98e052bdfbe42cc0fc51ad1a0075049f3
ThaiSmartDev <dev@thaismartdev.com>
2019-02-05 12:04:42 +0700

    Create serial.js

---
+++ objects/serial.js
@@ -0,0 +1,96 @@
+exports.firebaseUpdate = function (req, res) {
+    deleteData(req, res);
+};
+
+function deleteData(req, res) {
+    let msg = {
+        delete: {},
+        update: {}
+    }
+    let tp = require('tedious-promises');
+    tp.setConnectionConfig(require(`${__dirname}/../../.config/www/mssql.json`));
+    tp.sql(`EXEC Firebase..sp_SerialSyncData 'delete'`).execute().then(function (results) {
+        let all = results.length;
+        if (all > 0) {
+            let success = 0;
+            let arr = [];
+            results.forEach(result => {
+                db.ref(`serial/TH/0001/stock/${result.id}`).remove(function (error) {
+                    success++;
+                    if (error) {
+                        console.log(`Delete Serial ${result.id} Error (${error})`);
+                    } 
+                    else {
+                        arr.push(`'${result.id}'`);
+                        //console.log(`Delete Serial ${result.id} Success (${success}/${all})`);
+                    }
+                    if (success >= all) {
+                        msg.delete = { success: true, result: arr.toString()};
+                        tp.sql(`DELETE FROM Firebase..Serial WHERE serial IN (${arr.toString()})`).execute();
+                        console.log(msg.delete);
+                        setTimeout(function () {
+                            updateData(req, res, msg);
+                        }, 1000);
+                    }
+                })
+            });
+        } else {
+            msg.delete = 'No Delete Data';
+            console.log(msg.delete);
+            updateData(req, res, msg);
+        }
+    }).fail(function (err) {
+        msg.delete = err;
+        console.log(msg.delete);
+        updateData(req, res, msg);
+    });
+}
+
+
+function updateData(req, res, msg) {
+    let tp = require('tedious-promises');
+    tp.setConnectionConfig(require(`${__dirname}/../../.config/www/mssql.json`));
+    tp.sql(`EXEC Firebase..sp_SerialSyncData 'update'`).execute().then(function (results) {
+        let all = results.length;
+        if (all > 0) {
+            let success = 0;
+            let arr = [];
+            results.forEach(result => {
+                db.ref(`serial/TH/0001/info/${result.id}`).update({
+                    billNo: ''+result.buyNo,
+                    keyBillProduct: ''+result.buyNo+':'+result.product,
+                    poDate: ''+result.receivedDate,
+                    product: ''+result.product,
+                });
+
+                db.ref(`serial/TH/0001/stock/${result.id}`).update({
+                    product: result.product
+                }, function (error) {
+                    success++;
+                    if (error) {
+                        console.log(`Update Serial ${result.id} Error (${error})`);
+                    } 
+                    else {
+                        arr.push(`'${result.id}'`);
+                        //console.log(`Update Serial ${result.id} Success (${success}/${all})`);
+                    }
+                    if(success >= all) {
+                        msg.update = { success: true, result: arr.toString()};
+                        res.send(msg);
+                        tp.sql(`UPDATE Firebase..Serial SET isSync = 1, syncDate = GETDATE() WHERE serial IN (${arr.toString()})`).execute();
+                        console.log(msg.update);
+                        return true;
+                    }
+                })
+            });
+        } else {
+            msg.update = 'No Update Data';
+            res.send(msg);
+            return true;
+        }
+    }).fail(function (err) {
+        msg.update = err;
+        res.send(msg);
+        return true;
+    });
+}
